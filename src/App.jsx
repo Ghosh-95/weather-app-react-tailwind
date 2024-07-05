326349b6c01c1e8722045ef1833e242d1ec512ce
Sushovan <sushovanghosh960@gmail.com>
2024-07-05 22:07:12 +0530

    add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { ContentLeft } from "./componenets/ContentLeft"
 import ContentRight from "./componenets/ContentRight"
+import Footer from "./componenets/Footer"
 import Header from "./componenets/Header"
 
 function App() {
@@ -10,11 +11,12 @@ function App() {
 
                 {/* change the grid cols if any error occurred */}
                 <article
-                    className="container relative grid gap-6 grid-cols-[minmax(0_1fr)] h-full overflow-y-auto before:content-[''] before:absolute before:bottom-0 before:h-[3rem] before:w-full before:pointer-events-none before:bg-gradient-1 before:-z-[1]">
+                    className="container relative grid gap-6 grid-cols-[minmax(0_1fr)] h-full overflow-y-auto before:content-[''] before:absolute before:bottom-0 before:left-0 before:h-[3rem] before:w-full before:pointer-events-none before:bg-gradient-1 before:-z-[1]">
 
                     <ContentLeft />
                     <ContentRight />
 
+                    <Footer />
                 </article>
             </main>
         </>
---
+++ src/componenets/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+    return (
+        <footer className="flex flex-wrap justify-center items-center text-surfaceVariantOn text-center gap-[1rem_2rem] mt-8">
+            <p className="text-body-3">Copyright &copy; by Sushovan. All rights reserved.</p>
+
+            <p className="text-body-3 flex flex-wrap justify-center items-center"> Powered by
+                <a href="https://openweathermap.org/api" title="Free Openweather API" target="_blank" rel="noopener">
+                    <img src="../src/assets/openweather.png" alt="Openweather API logo" width={150} height={30} loading="lazy" />
+                </a>
+            </p>
+        </footer>
+    )
+}
